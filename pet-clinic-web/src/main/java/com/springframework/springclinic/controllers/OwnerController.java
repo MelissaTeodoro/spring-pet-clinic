eb2785178ef2d8349f5f0227b279d602cd539183
Melissa de Miranda <melissa.miranda@mercadolivre.com>
2020-02-24 13:12:22 -0300

    Added layout. Closes #20

--- pet-clinic-web/src/main/java/com/springframework/springclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/springframework/springclinic/controllers/OwnerController.java
@@ -15,7 +15,7 @@ public class OwnerController {
         this.ownerService = ownerService;
     }
 
-    @RequestMapping({"/", "/index", "/owners/index.html"})
+    @RequestMapping({"", "/owners/index.html"})
     public String listOwners(Model model) {
         model.addAttribute("owners", ownerService.findAll());
 
