6ab2c6ccffec6356bbba019c3971fb450d03da77
Melissa de Miranda <melissa.miranda@mercadolivre.com>
2020-02-24 17:54:51 -0300

    Added PetTypeService. Closes #29

---
+++ pet-clinic-data/src/main/java/com/springframework/springclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.springframework.springclinic.services;
+
+import com.springframework.springclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
