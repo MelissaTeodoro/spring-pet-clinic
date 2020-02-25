1a7d353b3e22d7ef2bd92d55d2d150096463dba6
Melissa de Miranda <melissa.miranda@mercadolivre.com>
2020-02-24 22:03:21 -0300

    Added Specialties Map Impl. Closes #31.

---
+++ pet-clinic-data/src/main/java/com/springframework/springclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package com.springframework.springclinic.services;
+
+import com.springframework.springclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long>{
+}
